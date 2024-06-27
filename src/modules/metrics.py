3c6a781c8bad94b06bfbcd4efd2c31e7b5e1753a
mayor-of-geology <atolagbejoshua2@gmail.com>
2024-06-27 12:04:01 +0100

    modify text

--- src/modules/metrics.py
+++ src/modules/metrics.py
@@ -2,6 +2,6 @@ from sklearn.metrics import r2_score, mean_squared_error
 from numpy import sqrt
 
 def metrics(pred, gt, name):
-    print(f'The R2 score for Well {name}: {r2_score(pred, gt)}')
-    print(f'The RMSE for Well {name}: {sqrt(mean_squared_error(pred, gt))}')
+    print(f'The R2 score for {name}: {r2_score(pred, gt)}')
+    print(f'The RMSE for {name}: {sqrt(mean_squared_error(pred, gt))}')
     print('-'*50)
